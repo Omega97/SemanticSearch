9d1e74ce472f7044da60cd70a988a41f5d9209a1
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-03-21 21:42:59 +0100

    model with matrix correction

--- semanticsearch/src/embedding.py
+++ semanticsearch/src/embedding.py
@@ -13,17 +13,15 @@ Useful models:
 - paraphrase-TinyBERT-L6-v2 (medium)
 - paraphrase-multilingual-mpnet-base-v2 (multilingual)
 - paraphrase-xlm-r-multilingual-v1 (multilingual)
-- stsb-bert-base (large)
-- stsb-roberta-base (large)
-- stsb-roberta-large (very large)
-- stsb-mpnet-base-v2 (very large)
-- stsb-mpnet-large-v2 (very large)
 """
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
 
 class EmbeddingModel:
+    """
+    The EmbeddingModel class is used to convert text inputs into vector embeddings.
+    """
     def __init__(self, model_name="all-MiniLM-L6-v2"):
         """
         Initialize the embedding model.
@@ -40,6 +38,7 @@ class EmbeddingModel:
 
         Args:
             texts (list of str): List of text inputs.
+            normalize_embeddings (bool): Whether to normalize the embeddings.
 
         Returns:
             np.ndarray: Array of embeddings (shape: [num_texts, embedding_dim]).
@@ -47,3 +46,36 @@ class EmbeddingModel:
         return np.array(self.model.encode(texts,
                                           normalize_embeddings=normalize_embeddings,
                                           convert_to_numpy=True))
+
+
+class EmbeddingModelWithCorrection(EmbeddingModel):
+    """
+    The EmbeddingModelWithCorrection class is used to convert text inputs into
+    vector embeddings, and apply a correction matrix to the embeddings.
+    """
+    def __init__(self, correction_matrix, model_name="all-MiniLM-L6-v2"):
+        """
+        Initialize the embedding model with a correction matrix.
+
+        Args:
+            correction_matrix (np.ndarray): A correction matrix to apply to the embeddings.
+            model_name (str): The name of the SentenceTransformer model to use.
+        """
+        super().__init__(model_name)
+        self.correction_matrix = correction_matrix
+
+    def encode(self, texts, normalize_embeddings=True):
+        """
+        Converts a list of text inputs into vector embeddings and applies a correction matrix.
+
+        Args:
+            texts (list of str): List of text inputs.
+            normalize_embeddings (bool): Whether to normalize the embeddings.
+
+        Returns:
+            np.ndarray: Array of embeddings (shape: [num_texts, embedding_dim]).
+        """
+        embeddings = super().encode(texts, normalize_embeddings)
+        if self.correction_matrix is not None:
+            embeddings = np.dot(embeddings, self.correction_matrix)
+        return embeddings
